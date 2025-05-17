2aa653cd10e377ff2f563bade41402f0a838b62b
Faizrakhman Alikhan <240381@astanait.edu.kz>
2025-05-18 01:31:23 +0500

    Add abstract Search class

--- src/Search.java
+++ src/Search.java
@@ -1,3 +1,29 @@
-// TODO: Abstract class for search algorithms
+import java.util.*;
+
 public abstract class Search<T> {
+    protected Set<Vertex<T>> marked;
+    protected Map<Vertex<T>, Vertex<T>> edgeTo;
+    protected Vertex<T> source;
+
+    public Search(Vertex<T> source) {
+        this.source = source;
+        this.marked = new HashSet<>();
+        this.edgeTo = new HashMap<>();
+    }
+
+    public boolean hasPathTo(Vertex<T> destination) {
+        return marked.contains(destination);
+    }
+
+    public List<Vertex<T>> pathTo(Vertex<T> destination) {
+        if (!hasPathTo(destination)) return null;
+
+        List<Vertex<T>> path = new ArrayList<>();
+        for (Vertex<T> current = destination; current != source; current = edgeTo.get(current)) {
+            path.add(current);
+        }
+        path.add(source);
+        Collections.reverse(path);
+        return path;
+    }
 }
