e1a9f03e5b33e037352058db4850824a4298fa93
Faizrakhman Alikhan <240381@astanait.edu.kz>
2025-05-18 01:26:14 +0500

    Implement WeightedGraph using Vertex-based structure

--- src/WeightedGraph.java
+++ src/WeightedGraph.java
@@ -1,3 +1,24 @@
-// TODO: Implement WeightedGraph using Vertex-based adjacency
+import java.util.HashSet;
+import java.util.Set;
+
 public class WeightedGraph<T> {
+    private Set<Vertex<T>> vertices;
+
+    public WeightedGraph() {
+        this.vertices = new HashSet<>();
+    }
+
+    public void addVertex(Vertex<T> vertex) {
+        vertices.add(vertex);
+    }
+
+    public void addEdge(Vertex<T> source, Vertex<T> destination, double weight) {
+        source.addAdjacent(destination, weight);
+        vertices.add(source);      // в случае, если source ещё не добавлен
+        vertices.add(destination); // в случае, если destination ещё не добавлен
+    }
+
+    public Set<Vertex<T>> getVertices() {
+        return vertices;
+    }
 }
