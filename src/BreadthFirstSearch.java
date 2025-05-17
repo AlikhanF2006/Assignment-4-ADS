518411c9bb9b9ac4bafce36f273c7abe76b38cbd
Faizrakhman Alikhan <240381@astanait.edu.kz>
2025-05-18 01:31:51 +0500

    Implement BreadthFirstSearch

--- src/BreadthFirstSearch.java
+++ src/BreadthFirstSearch.java
@@ -1,3 +1,27 @@
-// TODO: BFS implementation
+import java.util.*;
+
 public class BreadthFirstSearch<T> extends Search<T> {
+
+    public BreadthFirstSearch(WeightedGraph<T> graph, Vertex<T> source) {
+        super(source);
+        bfs(graph, source);
+    }
+
+    private void bfs(WeightedGraph<T> graph, Vertex<T> start) {
+        Queue<Vertex<T>> queue = new LinkedList<>();
+        marked.add(start);
+        queue.add(start);
+
+        while (!queue.isEmpty()) {
+            Vertex<T> current = queue.poll();
+
+            for (Vertex<T> neighbor : current.getAdjacentVertices().keySet()) {
+                if (!marked.contains(neighbor)) {
+                    marked.add(neighbor);
+                    edgeTo.put(neighbor, current);
+                    queue.add(neighbor);
+                }
+            }
+        }
+    }
 }
